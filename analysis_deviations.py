f37eeba9b3aa435ccc561de71d80f73676c091f9
kaetosh <59330834+kaetosh@users.noreply.github.com>
2024-11-22 17:42:17 +0300

    Update analysis_deviations.py

--- analysis_deviations.py
+++ analysis_deviations.py
@@ -13,7 +13,8 @@ def revolutions_before_processing(df, file_excel, sign_1c):
         print('Нет строки ИТОГО в таблице, чтобы свериться!!!!!')
         return None
     else:
-        df_for_check = df[df[sign_1c] == 'Итого'][[sign_1c] + existing_columns].copy()
+        df_for_check = df[df[sign_1c] == 'Итого'][[sign_1c] + existing_columns].copy().tail(2).iloc[[0]]
+
         
         df_for_check.fillna(0, inplace = True)
         df_for_check['Сальдо_начало_до_обработки'] = ((df_for_check['Дебет_начало'] if 'Дебет_начало' in existing_columns else 0) 
@@ -21,8 +22,6 @@ def revolutions_before_processing(df, file_excel, sign_1c):
         
         df_for_check['Сальдо_конец_до_обработки'] = ((df_for_check['Дебет_конец'] if 'Дебет_конец' in existing_columns else 0) 
                                                      - (df_for_check['Кредит_конец'] if 'Кредит_конец' in existing_columns else 0))
-        
-        
         
         df_for_check['Оборот_до_обработки'] = ((df_for_check['Дебет_оборот'] if 'Дебет_оборот' in existing_columns else 0)
                                                - (df_for_check['Кредит_оборот'] if 'Кредит_оборот' in existing_columns else 0))
@@ -34,7 +33,7 @@ def revolutions_before_processing(df, file_excel, sign_1c):
         df_for_check = df_for_check.reset_index(drop=True)
 
         logger.info(f'{file_excel}: сформировали таблицу с оборотами в разрезе счетов до обработки')
-
+        
         return df_for_check
 
 def revolutions_after_processing(df, df_for_check, file_excel):
@@ -50,12 +49,14 @@ def revolutions_after_processing(df, df_for_check, file_excel):
     
     # Фильтрация столбцов, названия которых заканчиваются на "_до"
     do_columns = df.filter(regex='_до$')
+    do_columns = do_columns.loc[:, ~do_columns.columns.str.startswith('Количество_')]
     
     # Вычисление суммы значений этих столбцов
     sum_do_columns = do_columns.sum().sum()
     
     # Фильтрация столбцов, названия которых заканчиваются на "_до"
     ko_columns = df.filter(regex='_ко$')
+    ko_columns = ko_columns.loc[:, ~ko_columns.columns.str.startswith('Количество_')]
     
     # Вычисление суммы значений этих столбцов
     sum_ko_columns = ko_columns.sum().sum()
@@ -63,12 +64,14 @@ def revolutions_after_processing(df, df_for_check, file_excel):
     df_for_check_2['Оборот_после_обработки'] = sum_do_columns - sum_ko_columns
     df_for_check_2['Сальдо_конец_после_обработки'] = [(df['Дебет_конец'].sum() if 'Дебет_конец' in existing_columns else 0) - (df['Кредит_конец'].sum() if 'Кредит_конец' in existing_columns else 0)]
     df_for_check_2 = df_for_check_2.reset_index(drop=True)
-
+    df_for_check = df_for_check.reset_index(drop=True)
+    
     # Объединение DataFrame с использованием внешнего соединения
     merged_df = pd.concat([df_for_check, df_for_check_2], axis=1)
 
     # Заполнение отсутствующих значений нулями
     merged_df = merged_df.infer_objects().fillna(0)
+    
 
     # Вычисление разницы
     merged_df['Разница_сальдо_нач'] = merged_df['Сальдо_начало_до_обработки'] - merged_df['Сальдо_начало_после_обработки']
@@ -81,4 +84,5 @@ def revolutions_after_processing(df, df_for_check, file_excel):
 
     merged_df['Исх.файл'] = file_excel
     logger.info(f'{file_excel}: сформировали дополнительную таблицу с отклонениями до и после обработки')
+    
     return merged_df
