723b13afb909e2240d55ee0430cd86e938501055
kaetosh <59330834+kaetosh@users.noreply.github.com>
2024-11-22 17:43:09 +0300

    Update C_horizontal_structure.py

--- processing/C_horizontal_structure.py
+++ processing/C_horizontal_structure.py
@@ -13,15 +13,15 @@ def fill_level(row, prev_value, level, sign_1c) -> float:
 
 def horizontal_structure(df, file_excel, sign_1c):
 
-        
 # Инициализация переменной для хранения предыдущего значения
     prev_value = None
 
     # получим максимальный уровень иерархии
     max_level = df['Уровень'].max()
+
     
     if (max_level == 0 or df.empty):
-        logger.info(f'{file_excel}: файл пустой, не включаем в свод')
+        logger.info(f'{file_excel}: файл пустой или проблемный, не включаем в свод')
         return True, df
 
     # разнесем уровни в горизонтальную ориентацию в цикле
@@ -30,7 +30,8 @@ def horizontal_structure(df, file_excel, sign_1c):
         for j, row in df.iterrows():
             if row['Уровень'] == i:
                 prev_value = row[sign_1c]
+                if prev_value == 'Количество':
+                    prev_value = df.at[j-1, sign_1c]
             df.at[j, f'Level_{i}'] = prev_value
     logger.info(f'{file_excel}: разнесли уровни в горизонтальную иерархию')
-    
     return False, df
